5a2d4a92896379da8c53ccb7fd71b6d2b09be0f5
VaishviPanchal <78603593+VaishviPanchal@users.noreply.github.com>
2021-10-31 23:35:33 +0530

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,33 @@
+class Launcher{
+    constructor(bodyA,pointB) {
+        var options={
+            bodyA : bodyA,
+            pointB : pointB,
+            length : 10,
+            stiffness : 0.1
+        }
+        this.body=Constraint.create(options)
+        //console.log(this.body)
+        //console.log("Launcher reated")
+        World.add(world,this.body)
+    }
+
+    display(){
+        if (this.body.bodyA){
+            var bodyA = this.body.bodyA.position;
+            var pointB = this.body.pointB;
+            //console.log(this.body.bodyA.position.x);
+            push()
+            stroke(49,22,8)
+            strokeWeight(1.7)
+            line(bodyA.x,bodyA.y,pointB.x,pointB.y)
+            
+        }
+    }
+    fly(){
+        this.body.bodyA=null;
+    }
+    attach(body){
+		this.body.bodyA=body
+	}
+}
